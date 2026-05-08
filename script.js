866084fe1143abb5ccb20d1e12f214e144f0bcd9
Nguyen Tran <tran.nguyen.024@pslib.cz>
2026-05-08 20:33:34 +0200

    o produktu

--- script.js
+++ script.js
@@ -7,4 +7,13 @@ document.addEventListener('DOMContentLoaded', () => {
             nav.classList.toggle('nav--open');
         });
     }
+    // Accordion for product detail
+    const accordions = document.querySelectorAll('.product-detail__section--accordion');
+    
+    accordions.forEach(acc => {
+        const title = acc.querySelector('.product-detail__section-title');
+        title.addEventListener('click', () => {
+            acc.classList.toggle('product-detail__section--open');
+        });
+    });
 });
